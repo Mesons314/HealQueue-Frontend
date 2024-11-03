8e08a8cd755cbdd8fc17d1ae9ab5be28519d1dcb
Abhinav Dwivedi <abhinavdwivedi102@gmail.com>
2024-11-03 12:45:44 +0530

    Added

---
+++ android/app/src/main/kotlin/com/example/healfast01/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.healfast01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Heal Fast',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.lightBlueAccent,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Heal Fast'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 100),
+              child: Container(
+
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: const BorderRadius.all(Radius.circular(15)),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 6,
+                      )
+                    ]
+                ),
+                child: const Center(
+                  child: Text(
+                    'Welcome\nto\nHeal Fast',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold, color:Colors.red,),
+                  ),
+                ),
+              ),),
+
+            SizedBox(
+              width: 225,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: ElevatedButton(onPressed: (){
+                },style: ElevatedButton.styleFrom(
+                    elevation: 10,
+                    shadowColor: Colors.black
+                ),child: const Text('Register As User', style: TextStyle(fontSize: 18),),),
+              ),
+            ),
+
+            SizedBox(
+              width: 225,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 100),
+                child: ElevatedButton(onPressed: (){
+                },style: ElevatedButton.styleFrom(
+                    elevation: 10,
+                    shadowColor: Colors.black
+                ), child: const Text('Register As Doctor', style: TextStyle(fontSize: 18),),),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
